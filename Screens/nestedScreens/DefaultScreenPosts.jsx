f1ae41a09f1fe564e93745a9ff634e055388c18f
Klimch1k <114351322+Klimch1k@users.noreply.github.com>
2023-08-29 14:49:45 +0300

    Fix bugs

--- Screens/nestedScreens/DefaultScreenPosts.jsx
+++ Screens/nestedScreens/DefaultScreenPosts.jsx
@@ -1,4 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, {
+  useEffect,
+  useState,
+  useCallback,
+  useLayoutEffect,
+} from "react";
+import { useDispatch } from "react-redux";
 import {
   View,
   Text,
@@ -8,28 +14,71 @@ import {
   Image,
   Button,
 } from "react-native";
+import { useFocusEffect } from "@react-navigation/native";
 
 import { db } from "../../firebase/config";
 import { collection, getDocs } from "firebase/firestore";
+import { useSelector } from "react-redux";
+import { FontAwesome } from "@expo/vector-icons";
+import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { authSingOutUser } from "../../Redux/auth/authOperations";
 
-export const DefaultScreenPosts = ({ navigation, route }) => {
+export const DefaultScreenPosts = ({ navigation }) => {
   const [posts, setPosts] = useState([]);
+  const { login, email } = useSelector((state) => state.auth);
+
+  const dispatch = useDispatch();
 
   const getAllPost = async () => {
     const snapshot = await getDocs(collection(db, "posts"));
-    setPosts(
-      snapshot.docs.map((doc) => ({
+    const fetchedPosts = [];
+
+    for (const doc of snapshot.docs) {
+      const commentsSnapshot = await getDocs(
+        collection(db, `posts/${doc.id}/comments`)
+      );
+      const post = {
         ...doc.data(),
         id: doc.id,
-      }))
-    );
+        comments: commentsSnapshot.size,
+      };
+      fetchedPosts.push(post);
+    }
+
+    setPosts(fetchedPosts);
   };
 
-  console.log(posts);
+  console.log("posts", posts);
 
   useEffect(() => {
     getAllPost();
   }, []);
+
+  useFocusEffect(
+    useCallback(() => {
+      getAllPost();
+    }, [])
+  );
+
+  const handleLogout = () => {
+    console.log("выход");
+    dispatch(authSingOutUser());
+  };
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerRight: () => (
+        <View style={styles.logoutIconContainer}>
+          <MaterialCommunityIcons
+            name="logout"
+            size={24}
+            color="#BDBDBD"
+            onPress={handleLogout}
+          />
+        </View>
+      ),
+    });
+  }, [navigation]);
 
   return (
     <View style={styles.container}>
@@ -43,8 +92,11 @@ export const DefaultScreenPosts = ({ navigation, route }) => {
               justifyContent: "center",
             }}
           >
-            <Image source={{ uri: item.photo }} style={{ height: 240 }} />
-            <View>
+            <Image
+              source={{ uri: item.photo }}
+              style={{ height: 240, marginBottom: 10, borderRadius: 8 }}
+            />
+            <View style={styles.nameContainer}>
               <Text>{item.namePost}</Text>
             </View>
             <View>
@@ -54,12 +106,22 @@ export const DefaultScreenPosts = ({ navigation, route }) => {
                   navigation.navigate("Map", { location: item.location })
                 }
               />
-              <Button
-                title="go to comments"
+              <TouchableOpacity
+                style={styles.commentContainer}
                 onPress={() =>
-                  navigation.navigate("Comments", { postId: item.id })
+                  navigation.navigate("Comments", {
+                    postId: item.id,
+                    photo: item.photo,
+                  })
                 }
-              />
+              >
+                <FontAwesome
+                  name={item.comments ? "comment" : "comment-o"}
+                  size={24}
+                  color="#FF6C00"
+                />
+                <Text style={styles.commentCount}>{item.comments}</Text>
+              </TouchableOpacity>
             </View>
           </View>
         )}
@@ -73,5 +135,11 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingRight: 16,
     paddingLeft: 16,
+  },
+  nameContainer: {
+    marginBottom: 10,
+  },
+  logoutIconContainer: {
+    marginRight: 20,
   },
 });
